388721121ee3db35f57dd6c0e87d22d7c36f3476
Guannan Dong <gdong@caltech.edu>
2020-05-10 15:29:03 -0700

    separate MethodAssistant into modules

---
+++ MethodAssistant/CullingCriteriaInput.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 13:47:32 2020
+
+@author: guannandong
+"""
+
+# Culling criteria input class
+class CullingCriteriaInput:
+    pass
+    # def __init__(self, peakType, Formula, mass, mtol, unit):
+    #     self.peakType = peakType
+    #     self.chemicalFormula = Formula.formula
+    #     self.mass = mass
+    #     self.mtol = mtol
+    #     self.unit = unit
---
+++ MethodAssistant/IsotopologueInput.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 12:14:49 2020
+
+@author: guannandong
+"""
+
+# isotopologue type tuple
+isoType = ("Base","13C-substituted","D-substituted","18O-substituted",
+            "15N-substituted","17O-substituted","Others")
+
+# mass tolerance unit tuple
+mtolUnitType = ("ppm","mmu")
+
+# electron mass
+emass = 0.00054858
+
+# isotopologue input class
+class IsotopologueInput:
+    def __init__(self, peakType, Formula, mass, mtol, unit):
+        self.peakType = peakType
+        self.chemicalFormula = Formula.formula
+        self.mass = mass
+        self.mtol = mtol
+        self.unit = unit
